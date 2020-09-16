55763944136c24e79e18f2f174fade9c17097082
Jette Bakemeier <jette.bakemeier@thoughtworks.com>
2020-09-16 14:44:16 +0200

    Start over and put the deck in a edn file

--- src/black_jack/core.clj
+++ src/black_jack/core.clj
@@ -1,130 +1,27 @@
-(ns black-jack.core)
+(ns black-jack.core
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io])
+  (:import (java.io PushbackReader)))
 
-;#_(comment
-;  we need
-;  - a deck of cards
-;  - a user
-;  - a game
-;  actions
-;  - start a game
-;  - give cards
-;  - feedback to user
-;  - ask for action
-;  - execute the action
-;  - calculate if lost or won)
+(defn get-deck [file]
+  (when-let [url (or (io/resource file) (io/file file))]
+    (with-open [r (-> url io/reader PushbackReader.)]
+      (edn/read r))))
+
+(defn create-game-context [deck]
+  {:deck     deck
+   :player-1 (repeatedly 2 #(rand-nth deck))})
+
+(defn play []
+  (->> (get-deck "deck.edn")
+       (create-game-context)))
 
 (defn start-game? []
   (if (= (read-line) "go") true false))
-
-(def BLACK-JACK 21)
-
-(def COLORS ["heart" "diamond" "spade" "cross"])
-
-(defn user-wins []
-  (prn "CONGRATULATIONS, YOU WON!"))
-
-(defn user-loses []
-  (prn "OH NO, YOU LOST!"))
-
-(defn create-deck []
-  (let [without-special-cards (->> (range 2 11)
-                                   (map #(map (fn [param1] (hash-map (keyword (str param1 "-" %)) %)) COLORS))
-                                   (reduce concat)
-                                   vec)
-        special-cards (->> (map #(map
-                                   (fn [param1]
-                                     (hash-map
-                                       (keyword (str % "-" (reduce str (keys param1))))
-                                       (reduce str (vals param1))))
-                                   [{:K 10} {:Q 10} {:J 10} {:A 11}]) COLORS)
-                           (reduce concat))]
-    (concat without-special-cards special-cards)))
-
-(defn give-card [deck]
-  (rand-nth deck))
-
-; pure function
-(defn calculate-result [cards]
-  (->> cards
-       (map #(vals %))
-       flatten
-       (reduce +)))
-
-; pure function
-(defn create-result-object [user-cards new-card]
-  (let [all-cards (conj user-cards new-card)
-        result (calculate-result all-cards)]
-    {:result      result
-     :cards       all-cards
-     :end-of-game (if (>= result BLACK-JACK) true false)}))
-
-(defn show-cards-and-result [result-object]
-  (prn "Your cards are:")
-  (loop [i 0]
-    (when (< i (count (:cards result-object)))
-      (prn (nth (:cards result-object) i))
-      (recur (inc i))))
-  (prn (str "Score: " (:result result-object))))
-
-; TODO: user cards should be subtracted from the deck
-;(defn give-card [user-cards deck]
-;  (rand-nth deck))
-
-(defn ask-for-action []
-  (prn "What do you want to do next? 1. Get another card! (Other values will end the game)")
-  (read-line))
-
-(defn show-new-card [new-card]
-  (prn (str "Your new card is: " new-card)))
-
-(defn end-game-with [result]
-  (if (= BLACK-JACK result)
-    (user-wins)
-    (user-loses)))
-
-(defn translate-result-for-user [result-object]
-  (show-cards-and-result result-object)
-  result-object)
-
-(defn execute-round [user-cards]
-  (->> (create-deck)
-       (give-card user-cards)
-       (create-result-object user-cards)
-       (translate-result-for-user)))
-
-(defn play-round [result-object]
-  (let [action (ask-for-action)]
-    (if (= action "1")
-      (execute-round (:cards result-object))
-      (end-game-with (:result result-object)))))
-
-;(defn play []
-;  (let [deck (create-deck)
-;        user-card (give-card [] deck)
-;        result-of-round-one (execute-round [user-card])]
-;    (if (:end-of-game result-of-round-one)
-;      (end-game-with (:result result-of-round-one))
-;      (let [result-object (play-round result-of-round-one)]
-;        (loop [result (:result result-of-round-one)]
-;          (when (or (> result 0) (< result 21))
-;            (prn "result " result)
-;            (prn "You can continue!"))
-;          (recur (+ (- result (:result result-object)) result)))))))
-
-(defn play []
-  (let [deck (create-deck)
-        user-card (give-card [] deck)
-        result-of-round-one (execute-round [user-card])
-        result-of-round-two (if (:end-of-game result-of-round-one)
-                              (end-game-with (:result result-of-round-one))
-                              (play-round result-of-round-one))]
-    (if (:end-of-game result-of-round-two)
-      (end-game-with (:result result-of-round-two))
-      (play-round result-of-round-two))))
 
 (defn interface []
   (prn "Welcome to Black Jack!")
   (prn "If you want to start the game, type 'go'")
   (if (start-game?) (play) (prn "Bye, see you next time!"))
-  (prn "Wanna play again? - type 'go'")
-  (if (start-game?) (play) (prn "Bye, see you next time!")))
+  #_(prn "Wanna play again? - type 'go'")
+  #_(if (start-game?) (play) (prn "Bye, see you next time!")))
--- test/black_jack/core_test.clj
+++ test/black_jack/core_test.clj
@@ -1,68 +1,73 @@
 (ns black-jack.core-test
   (:require [clojure.test :refer :all]
-            [black-jack.core :as core :refer :all]))
+            [black-jack.core :as core :refer :all]
+            [mock-clj.core :as mock]))
 
 (def a-deck [{:heart-2 2}
              {:diamond-2 2}
              {:spade-2 2}
-             {:cross-2 2}
+             {:club-2 2}
              {:heart-3 3}
              {:diamond-3 3}
              {:spade-3 3}
-             {:cross-3 3}
+             {:club-3 3}
              {:heart-4 4}
              {:diamond-4 4}
              {:spade-4 4}
-             {:cross-4 4}
+             {:club-4 4}
              {:heart-5 5}
              {:diamond-5 5}
              {:spade-5 5}
-             {:cross-5 5}
+             {:club-5 5}
              {:heart-6 6}
              {:diamond-6 6}
              {:spade-6 6}
-             {:cross-6 6}
+             {:club-6 6}
              {:heart-7 7}
              {:diamond-7 7}
              {:spade-7 7}
-             {:cross-7 7}
+             {:club-7 7}
              {:heart-8 8}
              {:diamond-8 8}
              {:spade-8 8}
-             {:cross-8 8}
+             {:club-8 8}
              {:heart-9 9}
              {:diamond-9 9}
              {:spade-9 9}
-             {:cross-9 9}
+             {:club-9 9}
              {:heart-10 10}
              {:diamond-10 10}
              {:spade-10 10}
-             {:cross-10 10}
-             {:heart-:K 10}
-             {:heart-:Q 10}
-             {:heart-:J 10}
-             {:heart-:A 11}
-             {:diamond-:K 10}
-             {:diamond-:Q 10}
-             {:diamond-:J 10}
-             {:diamond-:A 11}
-             {:spade-:K 10}
-             {:spade-:Q 10}
-             {:spade-:J 10}
-             {:spade-:A 11}
-             {:cross-:K 10}
-             {:cross-:Q 10}
-             {:cross-:J 10}
-             {:cross-:A 11}])
+             {:club-10 10}
+             {:heart-king 10}
+             {:heart-queen 10}
+             {:heart-jack 10}
+             {:heart-ace 11}
+             {:diamond-king 10}
+             {:diamond-queen 10}
+             {:diamond-jack 10}
+             {:diamond-ace 11}
+             {:spade-king 10}
+             {:spade-queen 10}
+             {:spade-jack 10}
+             {:spade-ace 11}
+             {:club-king 10}
+             {:club-queen 10}
+             {:club-jack 10}
+             {:club-ace 11}])
 
 (deftest create-deck-test
   (testing "should create a deck with the numbers 2-10, A, J, Q, K for all four colors"
-    (is (= a-deck (core/create-deck)))))
+    (is (= a-deck (core/get-deck "deck.edn")))))
 
-(deftest give-card-test
-  (testing "should take a random card from the deck"
-    (is (= true (map? (core/give-card a-deck))))))
+(deftest create-game-context-test
+  (testing "should return a game context with deck and player cards"
+    (mock/with-mock [rand-nth {:heart-2 2}]
+                    (is (= {:deck     a-deck
+                            :player-1 [{:heart-2 2}
+                                       {:heart-2 2}]} (core/create-game-context a-deck))))))
 
-(deftest calculate-result-test
-  (testing "should calculate value of user cards"
-    (is (= 20 (core/calculate-result [{:diamond-8 8} {:cross-9 9} {:spade-3 3}])))))
+
+
+
+
